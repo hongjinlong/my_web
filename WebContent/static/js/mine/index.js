f0a5aa290a1cf840630460bf09c17d667e6d7988
hongjinlong <hongjinlong@DESKTOP-6HNGH0U>
2017-08-13 11:37:13 +0800

    complete the base frame of website

--- WebContent/static/js/mine/index.js
+++ WebContent/static/js/mine/index.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+	$.get('static/html/top.html',function(data){
+		$('#top').html(data)
+	})
+	
 	$.get('static/html/left.html',function(data){
 		$('#left').html(data)
+	})
+	
+	$.get('static/html/bottom.html',function(data){
+		$('#bottom').html(data)
 	})
 })
